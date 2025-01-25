f9eea7ce25a865e8291db685386152a0fe9bbd63
Ekolinnia <89620061+Ekolinnia@users.noreply.github.com>
2025-01-24 20:51:17 -0500

    first commit

---
+++ speech_to_text.py
@@ -0,0 +1,109 @@
+#pip install sounddevice - Micorphone du PC
+#pip install vosk - convert input mic to text 
+#pip install queue
+#https://stackoverflow.com/questions/79253154/use-vosk-speech-recognition-with-python
+
+
+import pickle
+import vosk
+import sounddevice as sd
+import json
+#store the transcript
+import queue
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+#print out available device to speak on 
+print(sd.query_devices())
+
+#Load the tokenizer and the trained model
+#read in byte 
+with open("tokenizer.pickle", "rb") as handling:
+    commmand_tokenizer = pickle.load(handling)
+    
+tensor_model = tf.keras.models.load_model("command_classification_model.h5")
+
+
+
+# Initialize Vosk model dowloaded in 
+try:
+    model = vosk.Model("vosk-model-small-en-us-0.15") 
+    print("Model has loaded successfully")
+except Exception as e:
+    print(f"Error loading model: {e}")
+    exit(1)
+# Initilaize regonization model 
+recognizer = vosk.KaldiRecognizer(model, 16000)
+
+#Queue to store all transcription 
+trasncript_queue = queue.Queue()
+
+
+#Process captured input
+def audio_callback(indata, frames, time, status):
+    if status:
+        print(f"Stream status error: {status}")
+        return
+    try:
+        # Convert indata to bytes
+        if recognizer.AcceptWaveform(bytes(indata)):
+            result = json.loads(recognizer.Result())
+            #Get the transcription 
+            transcription = result.get('text', '')
+            print(f"Transcription: {transcription}")
+            if transcription:
+                #add transcription to queue
+                trasncript_queue.put(transcription)
+    except Exception as e:
+        print(f"Error in callback: {e}")
+
+
+#transcribe captured input into text
+def process_transcript():
+    print("Listening... Speak into macbook microphone!")
+    try:
+        with sd.RawInputStream(device=0,samplerate=16000, blocksize=1024, dtype='int16',
+                               channels=1, callback=audio_callback):
+            print("Press Ctrl+C to stop.")
+            #open a file 
+            with open("transcriptions.txt", "a") as file:  
+                while True:
+                    #retrieve from the queue
+                    transcription = trasncript_queue.get()
+                    if transcription:
+                        #process the transcript
+                        #test
+                        #sample_command = ["Take me to the nearest coffee shop"]
+                        print("This is test : ")
+                        print(transcription)
+                        # convert trasncription as a single string
+                        new_sequence = commmand_tokenizer.texts_to_sequences([transcription])
+                        new_padded_sequences = pad_sequences(new_sequence,maxlen=10, padding="post")
+                        
+                        #predict the command with th emodel
+                        prediction = tensor_model.predict(new_padded_sequences)
+                        #take the one with the highest predcition %
+                        predicted_label = tf.argmax(prediction[0]).numpy()
+                        
+                        print(f"Predicted command: {predicted_label} " )
+                        
+                        
+                        #save transcription in a file
+                        file.write(transcription + "\n")
+    except KeyboardInterrupt:
+        print("Stopped listening.")
+        
+        
+
+    
+
+# Start transcription
+process_transcript()
+
+
+
+
+
---
+++ training.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+#save token 
+import pickle
+
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+# Get datasheet (headers on the datasheet)
+data_train = pd.read_csv("commands1.csv", header=0)
+print(data_train.head())
+
+# Seperate Commands and Label for training
+commands = data_train["Command"].tolist()
+labels = data_train["Label"].tolist()
+
+#Convert the data (text command) into numbers (token) so that machine can understand it
+#https://docs.python.org/3/library/tokenize.html 
+#1.Create a Token dictionary that will fit with datasheet
+commands_token = Tokenizer(num_words = 1000,oov_token="<OOV>")
+commands_token.fit_on_texts(commands)
+
+#2.Converting the text into the sequence with the created token
+commands_sequences = commands_token.texts_to_sequences(commands)
+
+#3.Padding them so they have the same length of 10
+commands__padded_sequences = pad_sequences(commands_sequences,maxlen=10, padding="post")
+
+#printing the badded sequeces of command
+print( " \n Padded sequences: ")
+print(commands__padded_sequences)
+
+#NN model for text 
+model = tf.keras.Sequential([
+    #Text Embedding input layer (input 10, cuz command sequence is 10)
+    tf.keras.layers.Embedding(1000,16,input_length=10),
+    #Reduce Dimensions
+    tf.keras.layers.GlobalAveragePooling1D(),
+    #hidden layer
+    tf.keras.layers.Dense(16,activation = "relu"),
+    #ouput layer
+    tf.keras.layers.Dense(len(set(labels)), activation="softmax")
+])
+
+#Opdimize model
+model.compile(optimizer= "adam", loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+
+#Training the model
+print("\nTraining the model...")
+#30 epochs
+model.fit(commands__padded_sequences, np.array(labels), epochs=10, batch_size=2)
+
+#Save the trained model
+model.save("command_classification_model.h5")
+print("saved model")
+
+#save the token 
+with open("tokenizer.pickle", "wb") as handle:
+    pickle.dump(commands_token, handle, protocol=pickle.HIGHEST_PROTOCOL)
+print("Tokenizer saved to 'tokenizer.pickle'")
+
+#test the model with a command
+
+#command given
+new_command = [" drive me to hospital"]
+#convert the command into the token
+new_sequence = commands_token.texts_to_sequences(new_command)
+#padd the sequece
+new_padded = pad_sequences(new_sequence, maxlen=10, padding="post")
+
+
+# Predict
+prediction = model.predict(new_padded)
+predicted_label = tf.argmax(prediction[0]).numpy()
+
+print("\nPrediction:")
+print(f"Command: {new_command[0]}")
+print(f"Predicted Label: {predicted_label}")
